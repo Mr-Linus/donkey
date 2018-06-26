725397eca9b35f988f80c393e4136370806e8e31
unknown <708863861@qq.com>
2018-06-26 21:50:27 +0800

    test

--- main.go
+++ main.go
@@ -12,21 +12,9 @@ func main() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
-		UidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 1234,
-				HostID:      0,
-				Size:        1,
-			},
-		},
-		GidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 1234,
-				HostID:      0,
-				Size:        1,
-			},
-		},
 	}
+	cmd.SysProcAttr.Credential = &syscall.Credential{
+		Uid: uint32(1), Gid: uint32(1)}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
