2dc1ac18dd830a73c3e70054f856942738b0668a
unknown <708863861@qq.com>
2018-06-26 19:09:35 +0800

    fix main.go

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
-package main 
-
+package main
 
 import (
+	"log"
+	"os"
 	"os/exec"
 	"syscall"
-	"os"
-	"log"
 )
-
 
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-        Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
-            syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
-    }
-    cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1234,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1234,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
